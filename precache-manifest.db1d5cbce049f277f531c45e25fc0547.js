833ddca33d5bd2313a613c2431c556024d12e109
Krystian Kalwik <ambitious156288@gmail.com>
2021-03-07 21:13:04 +0100

    Updates

---
+++ precache-manifest.db1d5cbce049f277f531c45e25fc0547.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9efe1fd2166a704efa48",
+    "url": "/React-project/static/js/main.dfe7c1c8.chunk.js"
+  },
+  {
+    "revision": "8b2975c314b850aa349c",
+    "url": "/React-project/static/js/runtime~main.6ec83ef1.js"
+  },
+  {
+    "revision": "52b63e68079f3f81a5bb",
+    "url": "/React-project/static/js/2.9b6b1703.chunk.js"
+  },
+  {
+    "revision": "6fc1d8fe5a3f17e73f1ecd0d757475d1",
+    "url": "/React-project/static/media/link.6fc1d8fe.svg"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/React-project/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "af737b5505b4cdb4a433cc3ac1f234af",
+    "url": "/React-project/static/media/bulb.af737b55.svg"
+  },
+  {
+    "revision": "0969c92b498df082b2f18fc455b2fa13",
+    "url": "/React-project/static/media/logout.0969c92b.svg"
+  },
+  {
+    "revision": "051af8ba4d1984fc498ff6da805311cb",
+    "url": "/React-project/static/media/twitter.051af8ba.svg"
+  },
+  {
+    "revision": "84b70f8a7155a0edb0576db0efc9ace2",
+    "url": "/React-project/static/media/pen.84b70f8a.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/React-project/static/media/plus.e43ce6c8.svg"
+  },
+  {
+    "revision": "f1e8a2c5afe45446dcd1ab2af9985821",
+    "url": "/React-project/index.html"
+  }
+];
